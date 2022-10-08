2e172961281989df033e321fee5bccd385c8e269
王秀龙 <hzjsj@qq.com>
2022-10-08 22:27:03 +0800

    fix bug

--- eLiveDataApi/server.js
+++ eLiveDataApi/server.js
@@ -2,7 +2,8 @@ let Koa = require('koa')
 let Router = require('koa-router')
 const cors = require('@koa/cors');
 let fs = require('fs')
-
+const staticFiles = require('koa-static')
+const path = require('path')
 const app = new Koa()
 const router = new Router()
 
@@ -46,18 +47,18 @@ router.get('/orderData', async ctx => {
 router.get('/personalCenter', async ctx => {
 
   const data = {
-    code:200,
+    code: 200,
     data: {
-      userName:"111",
-      avatarUrl:"https://687a-hzpc-1258873690.tcb.qcloud.la/images/avatar-live.png",
-      userId:2489372598,
-      createTime:"2022-09-30",
-      authentication:true,
-      password:true,
-      question:false,
-      phoneNumber:"15055005050",
-      secureNumber:"150******50",
-      email:""
+      userName: "111",
+      avatarUrl: "https://687a-hzpc-1258873690.tcb.qcloud.la/images/avatar-live.png",
+      userId: 2489372598,
+      createTime: "2022-09-30",
+      authentication: true,
+      password: true,
+      question: false,
+      phoneNumber: "15055005050",
+      secureNumber: "150******50",
+      email: ""
     }
   }
   ctx.body = data
@@ -70,12 +71,15 @@ router.get('/', async ctx => {
 })
 
 // 将koa和中间件连起来
-app.use(cors()).use(router.routes()).use(router.allowedMethods());
+app.use(cors()).use(router.routes())
+app.use(router.allowedMethods())
 /**
  *  在加了router.allowedMethods()中间件情况下，
  *  如果接口是get请求，而前端使用post请求，会返回405 Method Not Allowed ，提示方法不被允许 ，并在响应头有添加允许的请求方式；
  *  而在不加这个中间件这种情况下，则会返回 404 Not Found找不到请求地址，并且响应头没有添加允许的请求方式 。
  */
+app.use(staticFiles(path.join(__dirname + './public/')));
+app.use(staticFiles('./public'));
 let port = 3000;
 app.listen(port, () => {
   console.log('server is running on ' + ' http://localhost:' + port)
